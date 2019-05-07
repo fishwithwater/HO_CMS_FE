a61d95506392b6467a66ce955957398b31f802bc
fishwithwater <1252055620@qq.com>
2019-05-07 19:37:36 +0800

    [FEATURE]swiper-item list && edit

--- src/config/stage/swiper-template.js
+++ src/config/stage/swiper-template.js
@@ -1,31 +1,58 @@
 const swiperTemplateRouter = {
   route: null,
   name: null,
-  title: '轮播模版',
-  type: 'tab', // 类型: folder, tab, view
+  title: '首页轮播',
+  type: 'folder', // 类型: folder, tab, view
   icon: 'el-icon-document-copy',
-  filePath: 'views/swiperTemplate', // 文件路径
+  filePath: 'views/', // 文件路径
   order: null,
   inNav: true,
   children: [
     {
-      title: '轮播模板列表',
-      type: 'view',
-      name: 'SwiperTemplateList',
-      route: '/swiper-item/list',
-      filePath: 'views/swiperTemplate/SwiperTemplateList.vue',
-      inNav: true,
-      icon: 'el-icon-document-copy',
+      title:'轮播项列表',
+      type:'tab',
+      name:null,
+      route:null,
+      filePath:'views/swiperItem/',
+      inNav:true,
+      icon:'el-icon-date',
+      children:[{
+        title:'轮播项列表',
+        type:'view',
+        name:'SwiperItemList',
+        route:'/swiper-item/list',
+        filePath:'views/swiperItem/SwiperItemList.vue',
+        inNav:true,
+        icon:'el-icon-date'
+      }]
     },
     {
-      title:'新增轮播模版',
-      type:'view',
-      name:'SwiperTemplateAdd',
-      route:'/swiper-item/add',
-      filePath:'views/swiperTemplate/SwiperTemplateAdd.vue',
-      inNav:false,
-      icon:'iconfont icon-add',
-      right:['创建轮播模版']
+      title: '轮播模板列表',
+      type: 'tab',
+      name: null,
+      route: null,
+      filePath: 'views/swiperTemplate/',
+      inNav: true,
+      icon: 'el-icon-document-copy',
+      children:[{
+        title: '轮播模板列表',
+        type: 'view',
+        name: 'SwiperTemplateList',
+        route: '/swiper-template/list',
+        filePath: 'views/swiperTemplate/SwiperTemplateList.vue',
+        inNav: true,
+        icon: 'el-icon-document-copy',
+      },
+        {
+          title:'新增轮播模版',
+          type:'view',
+          name:'SwiperTemplateAdd',
+          route:'/swiper-template/add',
+          filePath:'views/swiperTemplate/SwiperTemplateAdd.vue',
+          inNav:false,
+          icon:'iconfont icon-add',
+          right:['创建轮播模版']
+        }]
     }
   ],
 }
---
+++ src/views/swiperItem/SwiperItemList.vue
@@ -0,0 +1,150 @@
+<template>
+    <div>
+        <div v-if="!showEdit">
+            <!-- 列表页面 -->
+            <div class="container">
+                <div class="header">
+                    <div class="title">轮播项列表</div>
+                </div>
+                <!-- 表格 -->
+                <lin-table
+                        :tableColumn="tableColumn"
+                        :tableData="tableData"
+                        :operate="operate"
+                        @handleEdit="handleEdit"
+                        @handleDelete="handleDelete"
+                        v-loading="loading"></lin-table>
+            </div>
+            <!-- 分页 -->
+            <div class="pagination">
+                <el-pagination
+                        :background="true"
+                        :page-size="pageCount"
+                        :current-page="currentPage"
+                        v-if="refreshPagination"
+                        layout="prev, pager, next, jumper"
+                        :total="total_nums">
+                </el-pagination>
+            </div>
+        </div>
+        <!-- 编辑页面 -->
+        <swiper-item-edit v-else @editClose="editClose" :editSwiperItemId="editSwiperItemId"></swiper-item-edit>
+
+    </div>
+</template>
+
+<script>
+  import LinTable from '@/components/base/table/lin-table'
+  import SwiperItem from '@/lin/models/swiper-item'
+  import SwiperItemEdit from './SwiperItemEdit'
+
+  export default {
+    components: {
+      LinTable,
+      SwiperItemEdit
+    },
+    data() {
+      return {
+        refreshPagination: true, // 页数增加的时候，因为缓存的缘故，需要刷新Pagination组件
+        total_nums: 0, // 分组内的用户总数
+        currentPage: 1, // 默认获取第一页的数据
+        pageCount: 10, // 每页10条数据
+        tableData: [], // 表格数据
+        operate: [],
+        tableColumn: [{prop: 'id', label: '编号',width:'60'},
+          {prop: 'date', label: '日期',width:'100'},
+          {prop: 'dayOfWeek', label: '星期',width:'60'},
+          {prop: 'traditionalCalendar', label: '农历',width:'120'},
+          {prop: 'content', label: '内容',
+            formatter:(row,column,cellValue,index)=>{
+                return cellValue.substring(0,20)+'...'
+            }
+          },
+          {prop: 'footer', label: '页脚'},
+          {prop: 'img', label: '图片'},
+          {prop: 'url', label: '资源'},
+          {prop: 'template.name', label: '模板'},
+          {prop: 'template.displayName', label: '模板名称'},
+          {prop: 'author.nickname', label: '作者'},
+          {prop: 'month', label: '刊号'},
+        ], // 表头数据
+        loading: false,
+        showEdit: false,
+        editSwiperItemId: 0
+      }
+    },
+    methods: {
+      async getSwiperItemList() {
+        let res
+        const currentPage = this.currentPage - 1
+        try {
+          this.loading = true
+          res = await SwiperItem.getList({count: this.pageCount, page: currentPage})
+          this.loading = false
+          this.tableData = [...res.collection]
+          this.total_nums = res.total_nums
+        } catch (e) {
+          this.loading = false
+          console.log(e)
+        }
+      },
+      handleEdit(val) {
+        this.showEdit = true
+        this.editSwiperItemId = val.row.id
+      },
+      handleDelete(val) {
+        this.$confirm('此操作将永久删除该轮播项, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning',
+        }).then(async () => {
+          const res = await SwiperItem.delete(val.row.id)
+          if (res.error_code === 0) {
+            this.getSwiperItemList()
+            this.$message({
+              type: 'success',
+              message: `${res.msg}`,
+            })
+          }
+        })
+      },
+      editClose() {
+        this.showEdit = false
+        this.getSwiperItemList()
+      },
+    },
+    created() {
+      this.operate = [{name: '编辑', func: 'handleEdit', type: 'primary',auth:'修改轮播项'}, {
+        name: '删除', func: 'handleDelete', type: 'danger', auth: '删除轮播项',
+      }]
+      this.getSwiperItemList()
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+    .container {
+        padding: 0 30px;
+
+        .header {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+
+            .title {
+                height: 59px;
+                line-height: 59px;
+                color: $parent-title-color;
+                font-size: 16px;
+                font-family: PingFangSC-Medium;
+                font-weight: 500;
+            }
+        }
+
+        .pagination {
+            display: flex;
+            justify-content: flex-end;
+            margin: 20px;
+        }
+    }
+</style>
